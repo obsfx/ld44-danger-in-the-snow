4c05c8c3b651b577d932fd59bd066866eaf4f6da
obsfx <balandiomer@gmail.com>
2021-10-16 21:39:47 +0300

    Add build files

--- src/objects/tile-item.js
+++
@@ -1,8 +0,0 @@
-let TileItem = function (x, y, spriteName) {
-    Phaser.Sprite.call(this, game, x, y, spriteName, 1);
-
-    game.add.existing(this);
-}
-
-TileItem.prototype = Object.create(Phaser.Sprite.prototype);
-TileItem.prototype.constructor = TileItem;
